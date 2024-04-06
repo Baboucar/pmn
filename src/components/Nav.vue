56fc59eb2d73431ca3e2a9538087d9635dac3268
Baboucarr <baboucarr@Baboucarrs-MacBook-Pro.local>
2024-04-06 06:12:39 +0000

    fixed subnav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,57 +1,87 @@
 <template>
     <div>
-      <div class="flex_nav">
+     <div class="flex__desktopnav">
      <div class="mobile__header">
         <img class="mobile__logo" src="../assets/logo.png" alt="logo">
         <span class="harmburger__icon" @click="toggleNav">&#9776;</span>
-        </div>  
-        <nav v-show="isNavOpen" class="nav">
+       </div> 
+
+        <nav v-show="isNavOpen" class="nav  mobile__nav desktop__nav">
          <button class="close__button" @click="closeNav">X</button>
 
-         <div class="mobile_vertical_nav">
-            <!-- <button   class="main_link link_btn"> Actuell </button> -->
-            <!-- <button class="main_link link_btn"  @mouseover="openSubNav('subNav1')" @mouseleave="closeSubNav('subNav1')">Presentation</button> -->
-      
-            <!-- <div v-if="subNavStates.subNav1" class="innav">
-               <router-link to="/about">About</router-link>
-               <router-link to="">Presentsub</router-link>
-               <router-link to="">Presentsub</router-link>
-               <router-link to="">Presentsub</router-link>
-               <router-link to="">Presentsub</router-link>
-            </div> -->
-            <!-- <button class="main_link link_btn"  @mouseover="openSubNav('subNav2')" @mouseleave="closeSubNav('subNav2')">Presentation</button> -->
-      
-            <!-- <div v-if="subNavStates.subNav2" class="innav">
-               <router-link to="/about">About</router-link>
-               <router-link to="">Presentsub</router-link>
-               <router-link to="">Presentsub</router-link>
-               <router-link to="">Presentsub</router-link>
-               <router-link to="">Presentsub</router-link>
-            </div> -->
-            <!-- <button class="main_link link_btn"  >Presentation</button> -->
-      
-            <!-- <div v-if="subNavStates.subNav3" class="innav">
-               <router-link to="/about">About</router-link>
-               <router-link to="">Presentsub</router-link>
-               <router-link to="">Presentsub</router-link>
-               <router-link to="">Presentsub</router-link>
-               <router-link to="">Presentsub</router-link>
-            </div> -->
-            <router-link to="/" class="main_link link_btn">Actuel</router-link>
-            <router-link to="/about" class="main_link link_btn">About</router-link>
-            <router-link to="/moyen" class="main_link link_btn">Moyens</router-link>
+       
+           
+            <router-link to="/" class="main_link ">Actuel</router-link>
+            <router-link @click="toggleSubNav('subNav1')" to="#" class="main_link ">Groupe +</router-link>
+            <div class="sub__menu" v-show="subNavStates.subNav1">
+              <router-link class="sub__menu--links" to="/about">Link2</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+
+
+            </div>
+            <router-link to="#" @click="toggleSubNav('subNav2')" class="main_link ">Domain +</router-link>
+            <div class="sub__menu" v-show="subNavStates.subNav2">
+              <router-link class="sub__menu--links" to="/moyen">Moyen</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+
+
+            </div>
+            <router-link @click="toggleSubNav('subNav3')" to="#" class="main_link ">Products +</router-link>
+            <div class="sub__menu" v-show="subNavStates.subNav3">
+              <router-link class="sub__menu--links" to="/product">Products</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+
+
+            </div>
+            <router-link @click="toggleSubNav('subNav4')" to="#" class="main_link " >Activities +</router-link>
+            <div class="sub__menu" v-show="subNavStates.subNav4">
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+              <router-link class="sub__menu--links" to="">Link2</router-link>
+
+
+            </div>
+            <router-link to="/achievement" class="main_link " >Réalisations</router-link>
             
-            <router-link to="/product" class="main_link link_btn">Products</router-link>
-            <router-link to="/achievement" class="main_link link_btn" >Réalisations</router-link>
-            <router-link to="" class="main_link link_btn" >Presentation</router-link>
-            <router-link to="/contact" class="main_link link_btn" >Contact</router-link>
-
-
-          </div>
+            <router-link to="/contact" class="main_link " >Contact</router-link>
         
         </nav>
+       
       </div>
-    </div>
+      <div v-show="subNavStates.subNav1" class="submenu__desktop done">
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+      </div>
+      <div v-show="subNavStates.subNav2" class="submenu__desktop dtwo">
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+      </div>
+      <div v-show="subNavStates.subNav3" class="submenu__desktop dthree">
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+      </div>
+      <div v-show="subNavStates.subNav4" class="submenu__desktop dfour">
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+        <router-link class="sub__menu--links" to="">Link2</router-link>
+      </div>
+
+      </div>
+    
 </template>
 
 <script>
@@ -69,26 +99,23 @@ import { reactive, ref } from 'vue';
       subNav4:false
       // Add more sub-navigation states as needed
     });
-    function openSubNav(navKey) {
-            subNavStates[navKey] = true;
-        }
-
-        function closeSubNav(navKey) {
-            subNavStates[navKey] = false;
-        }
     function toggleSubNav(navKey) {
       subNavStates[navKey] = !subNavStates[navKey];
     }
+
     function toggleNav() {
       isNavOpen.value = !isNavOpen.value;
     }
+
     function closeNav() {
       isNavOpen.value = false;
     }
-    return { isNavOpen, subNavStates, openSubNav, closeSubNav, toggleNav, closeNav };
+
+    return { isNavOpen, subNavStates, toggleSubNav, toggleNav, closeNav };
+  }
 
   }
-    }
+    
 </script>
 
 
@@ -123,13 +150,16 @@ import { reactive, ref } from 'vue';
  }
  .main_link{
    border-bottom: 1px solid #FFF; /* 1px solid black line */
-  
-    &__a{
-      margin-top: 1rem;
-    }
-   
+   padding-left: 1rem;
+   display: block;
 
  }
+.sub__menu--links{
+  display: block;
+  font-size: .8rem;
+  padding-left: 2rem;
+
+}
  .harmburger__icon{
     margin-left: auto;
     font-size: 3rem;
@@ -137,15 +167,16 @@ import { reactive, ref } from 'vue';
  }
  li{
    list-style-type: none;
-   padding-top: .7rem;
+  
  }
  a{
    text-decoration: none;
    color: #FFF;
    font-size: 1rem;
-   display: block;
+   
    
  }
+
 
  /* Define the enter and leave animations */
 .fade-enter-active, .fade-leave-active {
@@ -158,20 +189,18 @@ nav{
   
    position: absolute;
    top: 0;
+   
    width: 100%;
    background: $color-primary;
-   height: 100%;
+   height: 4000px;
    z-index: 1;
 
-   
-
-  
+    
    
 }
-.mobile_vertical_nav{
+.mobile__nav{
    font-size: 1.7rem;
-   margin-top: 1.5rem;
-   margin-left: 1rem;
+  
 }
 
 .close__button {
@@ -186,69 +215,82 @@ nav{
    padding-top: 2rem;
  }
 
- .innav{
-  
+ .submenu__desktop{
+  display: none;
  }
 
  @media(min-width: 64rem){
    
    .nav{
-  
-
-   display: block !important;
    position: relative;
+   display: inline-block !important;
    height: auto;
-   width: 80%;
-   margin-left: 8rem;
-   display: flex;
+   top: 0;
    clip-path: polygon(10% 0, 100% 0, 100% 100%, 0 100%);
-
-
-  
    
    }
 
-   .mobile_vertical_nav{
-    margin-left: 4rem;
+   .desktop__nav{
+    width: 80%;
+    margin-left: auto;
    }
    
    .harmburger__icon{
       display: none;
    }
 
-   .mobile__header {
-      justify-content: space-between; /* Adjust as necessary */
-    }
+   
     .close__button {
       display: none; /* Hide close button on larger screens */
     }
-    .mobile_vertical_nav{
-      display: flex;
-      
-    }
-    .flex_nav{
-      display: flex;
-    }
+  
     .main_link{
+      position: relative;
       border-bottom: none;
-      padding:1rem ;
+      display: inline-block;
+      padding-left: 5rem;
+     
     }
-    a{
    
-      
- 
-    }
 
     .mobile__logo{
       max-width: 100%;
-    width: 191px;
-    height: 100px;
+    width: 165px;
+    height: 70px;
     margin-top: 0;
     }
+.flex__desktopnav{
+  display: flex;
+}
+.sub__menu{
+ display: none;
 
-    .innav{
-     
-    }
+}
+.submenu__desktop{
+  display: block;
+  position: absolute;
+  
+  top: 2.5rem;
+  width: 7rem;
+  z-index: 100;
+}
+.done{
+  left: 30rem; // Adjust based on design, could be 0 for a dropdown
+
+}
+.dtwo{
+  left: 40rem; // Adjust based on design, could be 0 for a dropdown
+
+}
+.dthree{
+  left: 50rem;
+}
+.dfour{
+  left: 60rem;
+}
+.sub__menu--links{
+  display: block;
+}
 
  }
 </style>
